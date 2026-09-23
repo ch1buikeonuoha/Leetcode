fb86bdc86b8216720bc9d6cfbb501be5fa7d240f
ch1buikeonuoha <chibuike42onuoha@outlook.com>
2026-09-23 20:50:28 +0100

    Add accepted solution for Rising Temperature

---
+++ 0197-rising-temperature/solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select today.id from Weather as today join Weather as yesterday on datediff(today.recordDate, yesterday.recordDate) = 1 where today.temperature > yesterday.temperature 
